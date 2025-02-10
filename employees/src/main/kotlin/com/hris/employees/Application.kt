0a750314c790f9435c104a896d022df6b60988de
nikile <abs@gmail.com>
2025-02-10 17:22:07 +0200

    adding monitoring tools

--- employees/src/main/kotlin/com/hris/employees/Application.kt
+++ employees/src/main/kotlin/com/hris/employees/Application.kt
@@ -4,20 +4,12 @@ import com.hris.employees.model.EmployeesService
 import com.hris.employees.monitoring.configureMonitoring
 import com.hris.employees.routes.registerRoutes
 import io.ktor.server.application.*
-import io.ktor.server.metrics.micrometer.*
 import io.ktor.server.netty.*
-import io.micrometer.core.instrument.binder.jvm.JvmGcMetrics
-import io.micrometer.core.instrument.binder.jvm.JvmMemoryMetrics
-import io.micrometer.core.instrument.binder.system.ProcessorMetrics
-import io.micrometer.core.instrument.distribution.DistributionStatisticConfig
-import io.micrometer.prometheus.PrometheusConfig
-import io.micrometer.prometheus.PrometheusMeterRegistry
 import org.jetbrains.exposed.sql.Database
 import org.kodein.di.DI
 import org.kodein.di.bind
 import org.kodein.di.instance
 import org.kodein.di.singleton
-import java.time.Duration
 
 fun main(args: Array<String>) {
     EngineMain.main(args)
@@ -27,7 +19,8 @@ fun Application.module() {
     registerRoutes(DI {
         bind<Database>() with singleton {
             Database.connect(
-                url = System.getenv("DATABASE_URL") ?: "jdbc:postgresql://localhost:5432/hris",
+                url = System.getenv("DATABASE_URL")
+                    ?: "jdbc:postgresql://localhost:5432/hris",
                 user = System.getenv("DATABASE_USER") ?: "postgres",
                 password = System.getenv("DATABASE_PASSWORD") ?: "Start#123"
             )
